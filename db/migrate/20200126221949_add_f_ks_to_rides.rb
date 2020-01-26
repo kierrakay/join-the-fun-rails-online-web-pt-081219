cd01aa938ec1bad2712de4cb44c30f557a979f3e
kierra <51520037+kierrakay@users.noreply.github.com>
2020-01-26 17:21:31 -0500

    Done.

---
+++ db/migrate/20200126221949_add_f_ks_to_rides.rb
@@ -0,0 +1,8 @@
+class AddFKsToRides < ActiveRecord::Migration[5.0]
+  def change
+    add_column :rides, :passenger_id, :integer
+    add_column :rides, :taxi_id, :integer
+  end
+end
+
+#add foreing keys to rides
